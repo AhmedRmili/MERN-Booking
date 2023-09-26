3480a6df7e6b7c39ddddcad5c5fb67d0470f78cc
ahmedrmili <ahmedrmili23@gmail.com>
2023-09-26 20:03:40 +0100

    add header and featuredProperties and propertyList

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import "./home.css"
+import Navbar from '../../components/navbar/Navbar'
+import Header from '../../components/header/Header'
+import Featured from '../../components/featured/Featured'
+import PropertyList from '../../components/propertyList/PropertyList'
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <div>
+          <Navbar/>
+          <Header/>
+          <div className="homeContainer">
+      <Featured/>
+      <h1 className='homeTitle'>Browse by property type</h1>
+      <PropertyList/>
+      <h1 className='homeTitle'>Homes guests love</h1>
+
+    </div>
+    </div>
+
+    )
 }
 
 export default Home
